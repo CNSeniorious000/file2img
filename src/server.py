eb4c0f1ce8fd7b83cd36936b8f9e4fbc8f4916a1
Muspi Merol <wjj123345567@icloud.com>
2023-11-06 12:18:39 +0800

    encode / decode from urls

--- src/server.py
+++ src/server.py
@@ -1,28 +1,52 @@
 from mimetypes import guess_type
-from typing import Literal
+from typing import Literal, Optional
 from urllib.parse import quote
 
 from fastapi import FastAPI, File, Response, UploadFile
+from httpx import Client
+from pydantic import HttpUrl
 
 from .core import decode, encode
 
 app = FastAPI()
+client = Client(http2=True)
 
 
-@app.post("/encode_file_to_image")
-def file2img(file: UploadFile = File(), file_format: Literal["webp", "png"] = "png"):
-    data_in = file.file.read()
-    data_out = encode(data_in, file_format, file.filename)
-    headers = {"x-size-input": str(len(data_in)), "x-size-out": str(len(data_out)), "content-disposition": f"inline; filename={quote(file.filename)}.{file_format}"}
+def _encode_data(data_in: bytes, file_format: str, filename: str):
+    data_out = encode(data_in, file_format, filename)
+    headers = {"x-size-input": str(len(data_in)), "x-size-out": str(len(data_out)), "content-disposition": f"inline; filename={quote(filename)}.{file_format}"}
     return Response(data_out, media_type=f"image/{file_format}", headers=headers)
 
 
-@app.post("/decode_image_to_file")
-def img2file(img: bytes = File()):
-    data, meta = decode(img)
+def _decode_data(data_in: bytes):
+    data, meta = decode(data_in)
     filename = meta.filename
     return Response(
         data,
         media_type=guess_type(filename)[0] if filename else "application/octet-stream",
         headers={"content-disposition": f"{'inline' if filename.startswith('text') else 'attachment'}; filename={quote(filename)}"} if filename else {},
     )
+
+
+@app.post("/encode")
+def file2img(file: UploadFile = File(), file_format: Literal["webp", "png"] = "png"):
+    data_in = file.file.read()
+    return _encode_data(data_in, file_format, file.filename)
+
+
+@app.post("/decode")
+def img2file(img: bytes = File()):
+    return _decode_data(img)
+
+
+@app.get("/encode")
+def url2img(url: HttpUrl, file_format: Literal["webp", "png"] = "png", filename: Optional[str] = None):
+    res = client.get(str(url))
+    filename = filename or (res.headers.get("content-disposition") or "").split(";", 1)[-1].split("=", 1)[-1].strip('"') or str(res.url).strip("/").rsplit("/", 1)[-1]
+    return _encode_data(res.read(), file_format, filename)
+
+
+@app.get("/decode")
+def url2file(url: HttpUrl):
+    data_in = client.get(str(url), headers={"accept": "image/*"}).read()
+    return _decode_data(data_in)
